8443c188f7416ac0a24764450ba5d2559f7bb4d6
Sharma30042000 <sagarsharma3042@gmail.com>
2021-06-23 16:55:27 +0530

    ok

--- string/longest_sub_stirng.py
+++ string/longest_sub_stirng.py
@@ -1,6 +1,12 @@
 def helper(s,i):
     if i<len(s)//2 and s[i]==s[i+1]:
          f=s[i]+s[i+1]
+         if i==1 and s[0]==s[3]:
+             return s[0]+f+s[3]
+         if i==1 and s[0]==s[2]:
+             return s[0]+f
+         if i==2 and s[0]==s[4] and s[1]==s[3]:
+             return s[0]+s[1]+f+s[4]
          for j in range(1,i+1):
              if s[i-j]==s[i+1+j]:
                  f=s[i-j]+f+s[i+1+j]
@@ -44,7 +50,7 @@ def ls(s):
         if len(f)>len(ans):
             ans=f
     return ans
-print(ls("abaxyzzyxf"))
+print(ls("aaaaabaegrffkdcc"))
            
 
 
