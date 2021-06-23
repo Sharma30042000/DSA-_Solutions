c62b597deb634f4e56b8dd613b4ab7244ac58341
Sharma30042000 <sagarsharma3042@gmail.com>
2021-06-23 12:10:11 +0530

    strng

--- string/Sparse_Matrix_Multiplication.py
+++ string/Sparse_Matrix_Multiplication.py
@@ -1,9 +1,19 @@
 def SparseMatrixMultiplication(a1,b1):
     if len(a1[0])!=len(b1):
         return [[]]
-    for i in len(a1):
-        c=0
-        for j in len(b1[0]):
-            if a1[i][c]==0 or b1[c][j]==0:
-                c=c+1
-            else:print("")
+    mat=[]
+    for i in range(len(a1)):
+            mat.append([])
+            
+    for i in range(len(a1)):
+        for j in range(len(b1[0])):
+            s=0
+            for k in range(len(b1)):
+                if a1[i][k]==0 or b1[k][j]==0:
+                    s=s
+                else:
+                    s=s+a1[i][k]*b1[k][j]
+            mat[i].append(s)
+    return mat
+print(SparseMatrixMultiplication([[0,2,0],[0,-3,5]],[[0,10,0],[0,0,0],[0,0,4]]))        
+
